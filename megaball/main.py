d91d36ccd69546d1b509823a79049f6322228113
Kryzle Camille <kryzlesayat@gmail.com>
2026-06-20 21:58:06 +0800

    upload of existing python project structure

---
+++ megaball/main.py
@@ -0,0 +1,41 @@
+# title: Megaball
+# author: Adam
+# desc: An arcade ball physics game
+# site: https://github.com/helpcomputer/megaball
+# license: MIT
+# version: 1.0
+
+import constants
+import game
+import input
+import pyxel
+
+
+class App:
+    def __init__(self):
+        pyxel.init(
+            constants.GAME_WIDTH,
+            constants.GAME_HEIGHT,
+            title=constants.GAME_TITLE,
+            fps=constants.GAME_FPS,
+        )
+
+        pyxel.load(constants.RESOURCE_FILE)
+        pyxel.images[0].load(0, 0, constants.IMAGE_BANK_0_FILE)
+
+        self.input = input.Input()
+        self.game = game.Game()
+        pyxel.mouse(False)
+        # pyxel.mouse(True)
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.input.get()
+        self.game.update(self.input)
+
+    def draw(self):
+        self.game.draw()
+
+
+App()
